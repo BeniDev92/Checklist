85dcd3971f32968505e90e114ce1b0441f9a6866
Beni <benignofo.apps@gmail.com>
2026-09-05 17:02:11 +0200

    Fix Stats Page from Vico error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,10 +35,10 @@ android {
 }
 
 dependencies {
-    val composeBom = platform("androidx.compose:compose-bom:2024.10.01")
+    val composeBom = platform("androidx.compose:compose-bom:2025.05.01")
     implementation(composeBom)
 
-    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
